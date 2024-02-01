ebbdb964231c3c0ad070095023789eb276071131
cdanielti <56508375+cdanielti@users.noreply.github.com>
2024-02-01 19:33:08 +0100

    se cambia el tipo de dato del parametro de la cla

--- src/main/java/com/app_salas/ApartadoSalas/ApartadoSalasApplication.java
+++ src/main/java/com/app_salas/ApartadoSalas/ApartadoSalasApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApartadoSalasApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(ApartadoSalasApplication.class, args);
 	}
 
